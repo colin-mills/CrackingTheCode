1909251bf854782242b1c8f3a49a865d21d4da87
Colin Mills <cgm71@georgetown.edu>
2022-06-12 14:49:09 -0500

    2.1 Remove duplicates problem

--- src/LinkedLists/LinkedList.java
+++ src/LinkedLists/LinkedList.java
@@ -1,5 +1,7 @@
 package LinkedLists;
 
+import java.util.HashSet;
+
 public class LinkedList {
 
     // Definition for singly-linked list as inner class.
@@ -11,8 +13,29 @@ public class LinkedList {
         Node(int val) { this.val = val; }
     }
 
+    //2.1 Remove Dups
+    //pg. 94
+    //TC: O(n)
+    //SC: O(1)
     public static Node removeDups(Node head) {
         //early exit / edge cases
         if (head == null || head.next == null) return head;
+        HashSet<Integer> visitedSet = new HashSet<>();
+        Node ptr = head;
+        visitedSet.add(head.val);
+        while (ptr != null && ptr.next != null) {
+            if (visitedSet.contains(ptr.val)) removeNext(ptr);
+            else {
+                visitedSet.add(ptr.next.val);
+                ptr = ptr.next;
+            }
+        }
+        return head;
+    }
+
+    private static void removeNext(Node prev) {
+        if (prev == null || prev.next == null) return;
+
+        prev.next = prev.next.next;
     }
 }
