d3ef77b7b6f743441bf0cf8989437ad7cdd71277
Colin Mills <cgm71@georgetown.edu>
2022-06-15 17:56:36 -0500

    3.1 Triple array stack constructors

---
+++ src/StacksAndQueues/ArrayTripleStack.java
@@ -0,0 +1,43 @@
+package StacksAndQueues;
+
+public class ArrayTripleStack {
+
+    //Stack Variables
+    int[] stack;
+    StackInfo[] metadata;
+
+    //metadata about each stack
+    private class StackInfo{
+        int startIndex;
+        int size;
+        int capacity;
+
+        StackInfo(int startIndex, int capacity) {
+            this.startIndex = startIndex;
+            this.capacity = capacity;
+            this.size = 0;
+        }
+
+        public int getTopIndex() {return startIndex + size;}
+        public boolean isFull() {return size == capacity;}
+        public boolean isEmpty() {return size == 0;}
+    }
+
+    ArrayTripleStack() {
+        this.stack = new int[27];
+        initializeStackInfo(27);
+    }
+
+    ArrayTripleStack(int capacity) {
+        this.stack = new int[capacity];
+        initializeStackInfo(capacity);
+    }
+
+    private void initializeStackInfo(int capacity) {
+        int extraSpace = capacity % 3, start = 0;
+        this.metadata = new StackInfo[3];
+
+        for (int i = 0; )
+    }
+
+}
