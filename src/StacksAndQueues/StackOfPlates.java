a85b291ffddea048b227248161d21ea04d99c8d5
Colin Mills <cgm71@georgetown.edu>
2022-06-19 17:35:02 -0500

    Stacks and Queues 3.3 Class

---
+++ src/StacksAndQueues/StackOfPlates.java
@@ -0,0 +1,17 @@
+package StacksAndQueues;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class StackOfPlates <T>  {
+    public List<Stack<T>> stacks = new ArrayList<>();
+    public int maxHeight = 10;
+    public int frontIndex = 0;
+
+    StackOfPlates (int maxHeight) {
+        this.maxHeight = maxHeight;
+    }
+
+
+}
