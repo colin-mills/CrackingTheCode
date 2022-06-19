df6cf4d7785d870b0be1bc89ed956b31f41618d8
Colin Mills <cgm71@georgetown.edu>
2022-06-19 18:00:15 -0500

    instatiate SortStack 3.5

---
+++ src/StacksAndQueues/SortStack.java
@@ -0,0 +1,21 @@
+package StacksAndQueues;
+
+import java.util.Stack;
+
+/*
+3.4 Sort Stack
+pg. 99
+Sort a stack where the lower values are on top,
+with only using one additional stack
+ */
+public class SortStack<T> extends Stack {
+    public void sort (Stack<T> stack) {
+        //buffer variable as we load values
+        T temp;
+        //this will have the bigger values on top
+        Stack<T> tempStack = new Stack<>();
+
+
+
+    }
+}
