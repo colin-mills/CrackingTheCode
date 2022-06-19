091828c9ce85129fe9f7e0000b0eac9a580db871
Colin Mills <cgm71@georgetown.edu>
2022-06-19 17:46:08 -0500

    Queue via two stacks implementation

---
+++ src/StacksAndQueues/MyQueue.java
@@ -0,0 +1,15 @@
+package StacksAndQueues;
+
+import java.util.Stack;
+
+public class MyQueue <T> {
+    Stack<T> stack = new Stack<>();
+    Stack<T> queue = new Stack<>();
+
+    public void push(T val) {
+        //doesn't matter if there are values in queue or not
+        //because we will just load more in the correct order
+        //when queue becomes empty
+        stack.push(val);
+    }
+}
