5dd7f829da04bf8247642885b898d3b751ccd4b4
Colin Mills <cgm71@georgetown.edu>
2022-06-19 19:48:30 -0500

    3.5 SortStack

--- src/StacksAndQueues/SortStack.java
+++ src/StacksAndQueues/SortStack.java
@@ -8,14 +8,23 @@ pg. 99
 Sort a stack where the lower values are on top,
 with only using one additional stack
  */
-public class SortStack<T> extends Stack {
+public class SortStack<T extends Comparable<T>> extends Stack {
     public void sort (Stack<T> stack) {
         //buffer variable as we load values
         T temp;
         //this will have the bigger values on top
         Stack<T> tempStack = new Stack<>();
-
-
-
+        while (!stack.isEmpty()) {
+            temp = stack.pop();
+            if (!tempStack.isEmpty()) {
+                //shift over all the values less than temp back to stack
+                while (temp.compareTo(tempStack.peek()) < 0) {
+                    stack.push(tempStack.pop());
+                }
+            }
+            tempStack.push(temp);
+        }
+        //reverse back to original stack
+        while (!tempStack.isEmpty()) stack.push(tempStack.pop());
     }
 }
