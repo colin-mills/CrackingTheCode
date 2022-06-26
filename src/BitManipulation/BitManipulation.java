873dfbd3294db94a483cae019938e1473dbe6f28
Colin Mills <cgm71@georgetown.edu>
2022-06-26 15:57:08 -0500

    draw line

--- src/BitManipulation/BitManipulation.java
+++ src/BitManipulation/BitManipulation.java
@@ -216,5 +216,36 @@ public class BitManipulation {
     TC: O(w) | where w is the width of the screen in bytes
     SC: O(1)
      */
+    public void drawLine(byte[] screen, int width, int x1, int x2, int y) {
+        int startOffset = x1 % 8;
+        int firstFullByte = x1 / 8;
 
+        if (startOffset != 0) firstFullByte++;
+
+        int endOffset = x2 % 8;
+        int lastFullByte = x2 / 8;
+        if (endOffset != 7) lastFullByte--;
+
+        //set full bytes
+        for (int b = firstFullByte; b <= lastFullByte; b++) screen[(width/8)* y + b] = (byte) 0xFF;
+
+        //create masks for start and end bytes
+        byte startMask = (byte) (0XFF >> startOffset);
+        byte endMask = (byte) ~(0XFF >> endOffset+1);
+
+        //set first and last
+        //first check if they are the same
+        if (x1/8 == x2/8) {
+            byte mask = (byte) (startMask & endMask);
+            screen[(width/8) * y + x1/8] |= mask;
+        } else {
+            if (startOffset != 0) {
+                int byteNumber = (width/8) * y + firstFullByte - 1;
+                screen[byteNumber] |= startMask;
+            } else if (endOffset != 7) {
+                int byteNumber = (width/8) * y + lastFullByte + 1;
+                screen[byteNumber] |= endMask;
+            }
+        }
+    }
 }
