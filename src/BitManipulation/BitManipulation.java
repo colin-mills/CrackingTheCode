b12dc9b42fa9a789dc75da1e102b728b24d865b7
Colin Mills <cgm71@georgetown.edu>
2022-06-26 13:04:24 -0500

    I got smarter

--- src/BitManipulation/BitManipulation.java
+++ src/BitManipulation/BitManipulation.java
@@ -21,9 +21,7 @@ public class BitManipulation {
         //find out how big the mask should be
         int size = msb - lsb + 1;
         //make the mask
-        //example: 2^3 = 8 decimal, 1000
-        // 1000 - 1 = 0111 which is a 3 digit mask
-        int mask = (int) Math.pow(2, size) -1;
+        int mask = (1 << lsb) - 1;
         //shift the mask over the start at the LSB and not it so the values from LSB to MSB, inclusive are 0;
         mask = ~(mask << lsb);
         return n & mask;
