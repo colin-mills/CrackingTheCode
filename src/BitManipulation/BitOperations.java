a45eeb1b64a873f48332d8964786be9c399c2ccd
Colin Mills <cgm71@georgetown.edu>
2022-06-26 11:50:54 -0500

    bitwise operators review

---
+++ src/BitManipulation/BitOperations.java
@@ -0,0 +1,15 @@
+package BitManipulation;
+
+public class BitOperations {
+    /*
+    Bit operators overview
+    & - bitwise and
+    | - bitwise or
+    ^ - bitwise Xor (exclusive or)
+    ~ - bitwise not
+    >> - right shift signed (arithmetic)
+    >>> - right shift unsigned (logical)
+    << - left shift
+     */
+
+}
