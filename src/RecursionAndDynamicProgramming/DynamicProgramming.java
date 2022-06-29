84882b5f0d9f9c3ea15d04f57219131b2dd45f03
Colin Mills <cgm71@georgetown.edu>
2022-06-28 19:52:15 -0500

    documentation

--- src/RecursionAndDynamicProgramming/DynamicProgramming.java
+++ src/RecursionAndDynamicProgramming/DynamicProgramming.java
@@ -7,6 +7,13 @@ public class DynamicProgramming {
         }
     }
 
+    /*
+    8.1 Triple Step
+    pg. 134
+    determine the number of ways you can reach the nth step if you can take a step of size 1, 2, or 3
+    TC: O(n)
+    SC: O(1)
+     */
     public static int tripleStep(int n) {
         int a = 0, b=0, c=1, d=1;
         for (int i=0; i <= n; i++) {
