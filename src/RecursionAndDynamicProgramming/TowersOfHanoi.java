e06ddf3c9bb093552f037d4f1c94ed167a32ffc7
Colin Mills <cgm71@georgetown.edu>
2022-06-29 10:21:20 -0500

    towers of hanoi object

---
+++ src/RecursionAndDynamicProgramming/TowersOfHanoi.java
@@ -0,0 +1,28 @@
+package RecursionAndDynamicProgramming;
+
+import java.util.Stack;
+
+public class TowersOfHanoi {
+    private class Tower {
+        private Stack<Integer> disks = new Stack<>();
+
+        public void add(int d) {
+            if (!disks.isEmpty() && disks.peek() <= d) {
+                System.out.println("Error placing disk " + d);
+            }
+            else disks.push(d);
+        }
+
+        public void moveTopTo(Tower t) {
+            int top = disks.pop();
+            t.add(top);
+        }
+
+        public void moveDisks(int quantity, Tower destination, Tower buffer) {
+            if (quantity <= 0) return;
+            moveDisks(quantity-1, buffer, destination);
+            moveTopTo(destination);
+            buffer.moveDisks(quantity - 1, destination, this);
+        }
+    }
+}
